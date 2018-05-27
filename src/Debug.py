ae1e9136667ae0af7c30a563e001c1519b45b119
Olivier L.-Applin <lapplin.olivier@gmail.com>
2018-05-27 14:02:02 -0400

    Start work on Macro class Change directory hierarchy to move outputs into seperate folder created programs folder

---
+++ src/Debug.py
@@ -0,0 +1,8 @@
+class Debug:
+
+    verbose = False
+    log_register = True
+    log_eval = False
+
+    def __init__(self, initial_state):
+        self.debug = initial_state
---
+++ src/Macro.py
@@ -0,0 +1,13 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+class Macro:
+
+    def __init__(self, ident, reglist, body):
+        self.ID = ident
+        self.reglist = reglist
+        self.body = body
+
+
+
+
